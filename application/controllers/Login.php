2c5541b98f721269cd417d0c1b8b89e14a05966b
Alan <90612014+alanmohan@users.noreply.github.com>
2023-04-24 22:52:56 +0400

    login feature added

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,9 +2,38 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-	
+
+	public function __construct() 
+    { 
+        parent::__construct(); 
+		$this->load->model('LoginModel');
+    }
 	public function index()
 	{
 		$this->load->view('dashboard_view.php');
+	}
+	public function login()
+	{
+		$this->load->view("login_view.php");
+	}
+	public function validate_user()
+	{
+		if(isset($_POST['userid']) && isset($_POST['password']))
+		{
+			$userid = $_POST['userid'];
+			$password = $_POST['password'];
+			$response=array();
+			if($this->LoginModel->validate_creds($userid,$password))
+			{
+				$response['status'] = 200;
+				$response['msg'] = 'login successful';
+			}
+			else
+			{
+				$response['status'] = 401;
+				$response['msg'] = 'invalid login';
+			}
+			echo json_encode($response);
+		}
 	}
 }
